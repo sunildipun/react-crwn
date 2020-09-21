7a3efdaf37538a0639a975927a2e2dcb364aa1ed
Sunil Behera <sunil@nubewell.com>
2020-09-21 09:58:45 +0530

    cart toggle hidden functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,10 @@ import { Link } from "react-router-dom";
 
 import { auth } from './../../firebase/firebase.utils';
 
-const Header = ({currentUser}) => (
+import CartIcon from './../cart-icon/cart-icon.component';
+import CartDropdown from './../cart-dropdown/cart-dropdown.component';
+
+const Header = ({currentUser, hidden}) => (
     <div className="header"> 
         <Link className="logo-container" to='/'>
             <Logo  className="logo"/>
@@ -21,12 +24,17 @@ const Header = ({currentUser}) => (
                 :
                 <Link className="option" to="/sign-in">SIGN IN</Link>
             }
-        </div>
+            <CartIcon />
+        </div> 
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 }) 
 
 export default connect(mapStateToProps)(Header);
