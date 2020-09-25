69044e3213ade8804c1eb660228df68f8232cc6c
sunildipun <sunil.6318ie@gmail.com>
2020-09-25 09:20:28 +0530

    updating our app with reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 
 import CartItem from './../cart-item/cart-item.component';
 import CustomButton from './../button/button.component'
+import { selectCartItems } from './../../redux/cart/cart.selector';
 
 
 const CartDropdown = ({cartItems}) => (
@@ -20,8 +21,8 @@ const CartDropdown = ({cartItems}) => (
 )
 
 
-const mapStateToProps = ({cart: { cartItems }}) => ({
-    cartItems
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import './cart-icon.style.scss';
 
 import {connect} from 'react-redux';
 import { toggleCartHidden } from './../../redux/cart/cart.action';
+import { selectCartItemsCount } from './../../redux/cart/cart.selector.js';
 
 import { ReactComponent as ShoppingIcon } from './../../assets/shopping-bag.svg';
 
@@ -20,8 +21,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    itemCount: cartItems.reduce((accumulatedQty, cartItem) => accumulatedQty + cartItem.quantity, 0)
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
